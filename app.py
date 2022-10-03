3ef7d2a022e9351c284aad6a4fa8e4e73d41fc79
PedrohDavi <pedro.henrique04896@gmail.com>
2022-10-02 23:18:08 -0300

    colocado Flask

---
+++ app.py
@@ -0,0 +1,22 @@
+from re import template
+from tkinter import Button
+from flask import Flask, render_template
+app = Flask("__name__")
+
+@app.route("/")
+@app.route("/home")
+def home ():
+    return render_template("home.html")
+
+
+@app.route("/quemsomos")
+def quemsomos():
+    return render_template("quemsomos.html")
+    
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+if __name__=='__main__':
+    app.run(debug=True)
