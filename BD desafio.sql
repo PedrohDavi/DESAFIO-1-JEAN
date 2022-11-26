2605b8a6cd41f57cd93a4030285b97b0aecbea77
PedrohDavi <pedro.henrique04896@gmail.com>
2022-11-26 13:43:53 -0300

    bd

--- BD desafio.sql
+++ BD desafio.sql
@@ -3,13 +3,10 @@ use contatos;
 
 create table contatos
 (
-	#id_contatos int primary key auto_increment,
-	email varchar(45) not null unique ,
-    nome varchar(45) not null,
-    assunto varchar(45) not null
-    
+email varchar(60) not null unique,
+nome varchar(45) not null,
+assunto varchar(45) not null
 );
-
 
 describe contatos;
 
@@ -17,7 +14,4 @@ insert into contatos (email, nome, assunto) values
 ('fatec@fatec.sp.gov.br','fatec','Flask'),
 ('fatec1@fatec.sp.gov.br','fatec','Banco de Dados');
 
-
 select * from contatos;
-
-
